4033f53e92b9095bee441d7e728784dcdab0e5e0
sagar <sagarmairrajput@gmail.com>
2023-12-22 02:02:50 +0530

    changes

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement is an object => when we render it on DOM it becomes HTML element
-// React.createElement =>  ReactElement-JS Object => HTMLElemnt(render)
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-
 //JSX
 // JSX => React.createElement =>  ReactElement-JS Object => HTMLElemnt(render)
-const jsxheading = <h1> JSX Namaste React! </h1>;
+const jsxheading = (
+  <h1 className="head" tabIndex="5">
+    JSX Namaste React!
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
