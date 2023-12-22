c70b25a9d08537c6d4567c5bc5da67d011ea7767
sagar <sagarmairrajput@gmail.com>
2023-12-22 10:06:20 +0530

    Functional COmponent understanding

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX
-// JSX => React.createElement =>  ReactElement-JS Object => HTMLElemnt(render)
-const jsxheading = (
+
+const Title = () => (
   <h1 className="head" tabIndex="5">
-    JSX Namaste React!
+    JSX Namaste React Title Compoenent!
   </h1>
 );
+
+
+const HeadingCmponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="head" tabIndex="5">
+      JSX Namaste React Heading Compoenent!
+    </h1>
+  </div>
+);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Whateverhappen in react it will happen under this root
 
-root.render(jsxheading);
+root.render(<HeadingCmponent />);
 
 
