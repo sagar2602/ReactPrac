6ee6f0be944e604da85136daf464908953f066bf
sagar <sagarmairrajput@gmail.com>
2023-12-22 18:04:20 +0530

    Real time Api Data usage for One Card only

--- App.js
+++ App.js
@@ -46,17 +46,87 @@ const styleCard = {
 }
 
 // Restaurant Card 
-const Card = ({ resName, resCuisine, resRate, resDel }) => {
+const Card = (props) => {
+  const { resData } = props;
   return (
     <div className="res-card" style={styleCard}>
       <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ilautxtm7p3tf9cibcnc"></img>
-      <h3>{ resName }</h3>
-      <h4>{ resCuisine }</h4>
-      <h4>{ resRate }</h4>
-      <h4>{ resDel }</h4>
+      <h3>{ resData.info.name }</h3>
+      <h4>{ resData.info.cuisines.join(", ") }</h4>
+      <h4>{ resData.info.avgRatingString } stars</h4>
+      <h4>{resData.info.sla.slaString }</h4>
     </div>
   )
 }
+
+const resObject = {
+  info: {
+    id: "140666",
+    name: "Burger King",
+    cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    locality: "ASJ Grand Plaza Mall",
+    areaName: "Dwarika Puri",
+    costForTwo: "₹350 for two",
+    cuisines: [
+      "Burgers",
+      "American"
+    ],
+    avgRating: 4.4,
+    parentId: "166",
+    avgRatingString: "4.4",
+    totalRatingsString: "5K+",
+    sla: {
+      deliveryTime: 18,
+      lastMileTravel: 1.5,
+      serviceability: "SERVICEABLE",
+      slaString: "18 mins",
+      lastMileTravelString: "1.5 km",
+      iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+      nextCloseTime: "2023-12-22 23:59:00",
+      opened: true
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {}
+      }
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹129"
+    },
+    loyaltyDiscoverPresentationInfo: {
+      logoCtx: {
+        logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+      },
+      freedelMessage: "FREE DELIVERY",
+      badgeType: "BADGE_TYPE_ONE_LITE"
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {}
+      }
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {}
+  },
+  analytics: {},
+  cta: {
+    link: "https://www.swiggy.com/restaurants/burger-king-asj-grand-plaza-mall-dwarika-puri-muzaffarnagar-140666",
+    type: "WEBLINK"
+  }
+};
+
 
 // Body Componenet 
 const Body = () => {
@@ -66,8 +136,7 @@ const Body = () => {
 
       </div>
       <div className="restaurant-container">
-        <Card resName="Bikkgane Biryani" resCuisine="Biryani, North Indian, Asian" resRate="4.3*" resDel="38Min"/>
-        <Card resName="KFC" resCuisine="Burger, Italian, Fast Food" resRate="4.9*" resDel="25Min"/>
+        <Card resData={ resObject } />
       </div>
     </div>
   )
