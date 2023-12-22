a3b4a2f6780e42883203b48403cf2b28fbe5912b
sagar <sagarmairrajput@gmail.com>
2023-12-22 10:18:59 +0530

    component with arrow function and without arrow function

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+// Functional component using Arrow function.
 const Title = () => (
   <h1 className="head" tabIndex="5">
     JSX Namaste React Title Compoenent!
   </h1>
 );
 
+// Functional component without using Arrow function.
+const Title2 = function () {
+  return (
+    <h1 className="head" tabIndex="5">
+      JSX Namaste React Title-2 Compoenent!
+    </h1>
+  )
+};
 
 const HeadingCmponent = () => (
   <div id="container">
     <Title />
+    <Title2 />
     <h1 className="head" tabIndex="5">
       JSX Namaste React Heading Compoenent!
     </h1>
