e2ac8babe8d6c5d1509f7bf5f91ae31daef213f2
sagar <sagarmairrajput@gmail.com>
2023-12-24 01:19:08 +0530

    Understand state variable concept i.e. concept of useState() react hook with 3 dummy restaurant data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,77 +1,84 @@
 import Card from "./Card";
 import resObject from "../utils/mockData";
+import { useState } from "react";
 
 // Body Componenet 
 const Body = () => {
-  let listOfRests = [
-    {
-      info: {
-        id: "140666",
-        name: "Burger King",
-        cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-        locality: "ASJ Grand Plaza Mall",
-        areaName: "Dwarika Puri",
-        costForTwo: "₹350 for two",
-        cuisines: [
-          "Burgers",
-          "American"
-        ],
-        avgRating: 4.4,
-        avgRatingString: "4.4",
-        sla: {
-          deliveryTime: 18,
-          slaString: "18 mins",
+  // Local State variable - superpoerful variable
+  const [ listOfRests, setListOfRestraunt ] = useState(
+    [
+      {
+        info: {
+          id: "140666",
+          name: "Burger King",
+          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+          locality: "ASJ Grand Plaza Mall",
+          areaName: "Dwarika Puri",
+          costForTwo: "₹350 for two",
+          cuisines: [
+            "Burgers",
+            "American"
+          ],
+          avgRating: 4.4,
+          avgRatingString: "4.4",
+          sla: {
+            deliveryTime: 18,
+            slaString: "18 mins",
+          },
         },
       },
-    },
-    {
-      info: {
-        id: "140667",
-        name: "KFC",
-        cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-        locality: "ASJ Grand Plaza Mall",
-        areaName: "Dwarika Puri",
-        costForTwo: "₹350 for two",
-        cuisines: [
-          "Burgers",
-          "American"
-        ],
-        avgRating: 4.5,
-        avgRatingString: "4.4",
-        sla: {
-          deliveryTime: 18,
-          slaString: "18 mins",
+      {
+        info: {
+          id: "140667",
+          name: "KFC",
+          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+          locality: "ASJ Grand Plaza Mall",
+          areaName: "Dwarika Puri",
+          costForTwo: "₹350 for two",
+          cuisines: [
+            "Burgers",
+            "American"
+          ],
+          avgRating: 4.5,
+          avgRatingString: "4.4",
+          sla: {
+            deliveryTime: 18,
+            slaString: "18 mins",
+          },
         },
       },
-    },
-    {
-      info: {
-        id: "140668",
-        name: "Burger King-duplicate",
-        cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-        locality: "ASJ Grand Plaza Mall",
-        areaName: "Dwarika Puri",
-        costForTwo: "₹350 for two",
-        cuisines: [
-          "Burgers",
-          "American"
-        ],
-        avgRating: 3.3,
-        avgRatingString: "3.3",
-        sla: {
-          deliveryTime: 18,
-          slaString: "18 mins",
+      {
+        info: {
+          id: "140668",
+          name: "Burger King-duplicate",
+          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+          locality: "ASJ Grand Plaza Mall",
+          areaName: "Dwarika Puri",
+          costForTwo: "₹350 for two",
+          cuisines: [
+            "Burgers",
+            "American"
+          ],
+          avgRating: 3.3,
+          avgRatingString: "3.3",
+          sla: {
+            deliveryTime: 18,
+            slaString: "18 mins",
+          },
         },
       },
-    },
-  ];
+    ]
+  );
   return (
     <div className="body-container">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log('clicked');
+            const topRatedList = listOfRests.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setListOfRestraunt(topRatedList);
           }}
         > Top Rated </button>
       </div>
