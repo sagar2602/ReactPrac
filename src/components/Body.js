a8f30971b1b2aed869ef6ff704b4c9a0ef996806
sagar <sagarmairrajput@gmail.com>
2023-12-24 01:25:21 +0530

    useState() hook with real mockData

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,70 +5,7 @@ import { useState } from "react";
 // Body Componenet 
 const Body = () => {
   // Local State variable - superpoerful variable
-  const [ listOfRests, setListOfRestraunt ] = useState(
-    [
-      {
-        info: {
-          id: "140666",
-          name: "Burger King",
-          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-          locality: "ASJ Grand Plaza Mall",
-          areaName: "Dwarika Puri",
-          costForTwo: "₹350 for two",
-          cuisines: [
-            "Burgers",
-            "American"
-          ],
-          avgRating: 4.4,
-          avgRatingString: "4.4",
-          sla: {
-            deliveryTime: 18,
-            slaString: "18 mins",
-          },
-        },
-      },
-      {
-        info: {
-          id: "140667",
-          name: "KFC",
-          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-          locality: "ASJ Grand Plaza Mall",
-          areaName: "Dwarika Puri",
-          costForTwo: "₹350 for two",
-          cuisines: [
-            "Burgers",
-            "American"
-          ],
-          avgRating: 4.5,
-          avgRatingString: "4.4",
-          sla: {
-            deliveryTime: 18,
-            slaString: "18 mins",
-          },
-        },
-      },
-      {
-        info: {
-          id: "140668",
-          name: "Burger King-duplicate",
-          cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-          locality: "ASJ Grand Plaza Mall",
-          areaName: "Dwarika Puri",
-          costForTwo: "₹350 for two",
-          cuisines: [
-            "Burgers",
-            "American"
-          ],
-          avgRating: 3.3,
-          avgRatingString: "3.3",
-          sla: {
-            deliveryTime: 18,
-            slaString: "18 mins",
-          },
-        },
-      },
-    ]
-  );
+  const [ listOfRests, setListOfRestraunt ] = useState(resObject);
   return (
     <div className="body-container">
       <div className="filter">
