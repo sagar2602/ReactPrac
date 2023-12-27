81fa53aee09efa3510b5ce6e166f2668d0ac3324
sagar <sagarmairrajput@gmail.com>
2023-12-28 04:06:17 +0530

    Adding configuration for About page/component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter } from "react-router-dom";
 
 // Build App Layout Component
 const AppLayout = () => {
@@ -12,6 +14,17 @@ const AppLayout = () => {
     </div>
   )
 }
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
