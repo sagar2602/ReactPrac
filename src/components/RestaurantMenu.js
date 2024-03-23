e58655e09ca10c168620338c97c02f61082af372
sagar <sagarmairrajput@gmail.com>
2024-03-23 17:20:48 +0530

    EP-7:- different menu items displayed with content

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -10,22 +10,48 @@ const RestaurantMenu = () => {
   const fetchMenu = async () => {
     const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.608637&lng=77.359795&restaurantId=403547&catalog_qa=undefined&submitAction=ENTER");
     const json = await data.json();
-    console.log(json);
     setResInfo(json.data);
   }
 
   if (resInfo === null) return <Shimmer />;
   const { name, cuisines, costForTwoMessage } = resInfo?.cards[ 0 ]?.card?.card?.info;
+  
+  // const { itemCards } = resInfo?.cards[ 2 ]?.groupedCard?.cardGroupMap?.REGULAR?.cards[ 1 ]?.card?.card;
+  // console.log(itemCards);
 
-  // const { cardsArray } = resInfo?.cards[ 2 ]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+  const cardsArray = resInfo?.cards[ 2 ]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+  // console.log(cardsArray);
 
 
   return (
     <div className="menu">
       <h1>{name}</h1>
       <p>{cuisines.join(", ")} - { costForTwoMessage }</p>
-      <h2></h2>
       <h2>Menu</h2>
+      {cardsArray.map((eachCard) => {
+        // console.log(eachCard.card.card[ '@type' ].includes('ItemCategory'));
+        // console.log(eachCard);
+        // console.log('============');
+        // console.log(eachCard.card.card['itemCards']);
+        if (eachCard.card.card[ '@type' ].includes('ItemCategory') && !eachCard.card.card[ '@type' ].includes('NestedItemCategory')) {
+          return (
+           <div className="item-category">
+              <h1>{eachCard.card.card[ 'title' ]}</h1>
+                          {/* const {itemCards} = eachCard.card.card; */}
+            <ul>
+              {eachCard.card.card['itemCards'].map((item) => (
+                <li key={item.card.info.id } className="item-details">
+                  <span className="title">{item.card.info.name}</span>
+                  <span className="cost">{item.card.info.price / 100}</span>
+                  <span className="rating">{item.card.info.ratings.aggregatedRating.rating}</span>
+                </li>
+              ))}
+            </ul>
+            </div>
+          )
+        }
+  })}
       <ul>
         <li>Biryani</li>
         <li>Burgers</li>
