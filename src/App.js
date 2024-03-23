7d61264c5ac8e38260cfad7346722c87391d077f
sagar <sagarmairrajput@gmail.com>
2024-03-24 05:03:17 +0530

    EP-9: Lazy loading in react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -7,6 +7,9 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
+
+// Lazy Loading for Grocery Components and sub components.
+const Grocery = lazy(() => import("./components/Grocery"));
 
 // Build App Layout Component
 const AppLayout = () => {
@@ -37,6 +40,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: <Grocery />,
       },
       {
         path: "/restaurants/:resId",
---
+++ src/components/Grocery.js
@@ -0,0 +1,5 @@
+const Grocery = () => {
+  return <h1>A lot of components resides here.</h1>
+}
+
+export default Grocery;
