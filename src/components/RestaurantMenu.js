79f49cf9dcbf2de1d6853d166243391cd46f8dbe
sagar <sagarmairrajput@gmail.com>
2024-03-24 23:31:11 +0530

    EP-10: Added all styles by tailwind css as it was before to the restaurant detail page

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -12,19 +12,19 @@ const RestaurantMenu = () => {
 
   return (
     <div className="menu">
-      <h1>{name}</h1>
-      <p>{cuisines.join(", ")} - { costForTwoMessage }</p>
-      <h2>Menu</h2>
+      <h1 className="px-4 m-1 font-bold">{name}</h1>
+      <p className="px-4 m-1">{cuisines.join(", ")} - { costForTwoMessage }</p>
+      <h2 className="p-4 m-4">Menu...</h2>
       {cardsArray.map((eachCard, index) => {
         if (eachCard.card.card[ '@type' ].includes('ItemCategory') && !eachCard.card.card[ '@type' ].includes('NestedItemCategory')) {
           // Destructuring to make it easy to read.
           const { title, itemCards } = eachCard.card.card;
           return (
            <div className="item-category" key={index}>
-              <h1>{title}</h1>
+              <h1 className="p-4 m-4 font-bold">{title}</h1>
             <ul>
               {itemCards.map((item) => (
-                <li key={item.card.info.id } className="item-details">
+                <li key={item.card.info.id } className="item-details flex justify-between border border-solid border-black">
                   <span className="title">{item.card.info.name}</span>
                   <span className="cost">{item.card.info.price / 100 || item.card.info.defaultPrice / 100}</span>
                   <span className="rating">{item.card.info.ratings.aggregatedRating.rating}</span>
@@ -38,14 +38,14 @@ const RestaurantMenu = () => {
           const { title, categories } = eachCard.card.card;
           return (
             <div className="item-nested-category" key={index}>
-              <h1>{"**Nested**" + title}</h1>
+              <h1 className="p-4 m-4 font-bold">{"**Nested**" + title}</h1>
                {categories.map((eachCategory, i) => 
                  <div className="item-nested-category" key={i}>
-                    <h2>{"**" + eachCategory['title'] + "**"}</h2>
+                    <h2 className="p-4 m-4 font-semibold">{"**" + eachCategory['title'] + "**"}</h2>
                     <ul>
                      {eachCategory[ 'itemCards' ].map((iCards) =>
                        (
-                        <li key={iCards.card.info.id } className="item-details">
+                        <li key={iCards.card.info.id } className="item-details flex justify-between border border-solid border-black">
                           <span className="title">{iCards.card.info.name}</span>
                           <span className="cost">{iCards.card.info.price / 100 || iCards.card.info.defaultPrice / 100}</span>
                           <span className="rating">{iCards.card.info.ratings?.aggregatedRating?.rating || 0.00 }</span>
