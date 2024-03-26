84e4a2cb30480014ad36ef127871422e95a121e1
sagar <sagarmairrajput@gmail.com>
2024-03-26 19:12:48 +0530

    EP-10: Rest detail page : refactored by adding css

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -7,15 +7,61 @@ const RestaurantMenu = () => {
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId);
   if (resInfo === null) return <Shimmer />;
-  const { name, cuisines, costForTwoMessage } = resInfo?.cards[ 0 ]?.card?.card?.info;
+  const cardsDetails = resInfo?.cards;
 
-  const cardsArray = resInfo?.cards[ 2 ]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+  const cardsArray = resInfo?.cards[ 4 ]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
 
   return (
     <div className="menu">
-      <h1 className="px-4 m-1 font-bold">{name}</h1>
-      <p className="px-4 m-1">{cuisines.join(", ")} - { costForTwoMessage }</p>
-      <h2 className="p-4 m-4">Menu...</h2>
+      {cardsDetails.map((eachC, i) => {
+        if (eachC?.card?.card[ '@type' ].includes('swiggy.presentation.food.v2.Restaurant')) {
+          const { id,name,cuisines,areaName,sla,avgRatingString,totalRatingsString,feeDetails,costForTwoMessage,gridElements } = eachC?.card?.card['info']
+          return (
+            <div className="widget-card mx-[200px]">
+              <span className="font-bold text-xl">{name}</span>
+              <div className="w-card flex justify-between">
+                <div className="w-left-card flex flex-col">
+                  <span className="">{name}</span>
+                  <span className="">{cuisines.join(', ')}</span>
+                  <span className="">{areaName + ', ' + sla.lastMileTravelString}</span>
+                </div>
+                <div className="w-right-card flex flex-col">
+                  <span className="">{'*' + avgRatingString}</span>
+                  <span className="">{totalRatingsString}</span>
+                </div>
+              </div>
+              <span className="delievery">{"bike-symbol " + feeDetails.message}</span>
+              <div className="offer-details">
+                <div className="flex w-1/2 justify-between">
+                  <span className="pp">{"progress-bar " + sla.slaString}</span>
+                  <span className="pp1">{"Rs. " + costForTwoMessage}</span>
+                </div>
+              </div>
+            <div className=""><h2 className="font-bold">Menu...</h2></div>
+            </div>
+          )
+        }
+        if (eachC?.card?.card[ '@type' ].includes('swiggy.gandalf.widgets.v2.GridWidget') && eachC?.card?.card?.gridElements.infoWithStyle[ '@type' ].includes('swiggy.presentation.food.v2.OfferInfoWithStyle')) {
+          const { offers } = eachC?.card?.card?.gridElements.infoWithStyle;
+          return (<div className="mx-[200px] flex">{
+            offers.map((eachOffer, io) => {
+              return (
+                <div className="border border-solid border-black w-1/3 rounded-lg mr-2 p-4">
+                  <div className="flex">
+                    <img className="rounded-lg mr-2" src={ "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_28,h_28/" + eachOffer.info.logoBottom }></img>
+                    <span className={"text-green-500" }>{eachOffer.info.header}</span>
+                  </div>
+                  <div className="flex">
+                    <span className="mr-2">{eachOffer.info.couponCode}</span>
+                    <span className="mr-2">{ "|" }</span>
+                    <span>{ eachOffer.info.description }</span>
+                  </div>
+                </div>
+              )
+            })
+          }</div>)
+        }
+      })}
       {cardsArray.map((eachCard, index) => {
         if (eachCard.card.card[ '@type' ].includes('ItemCategory') && !eachCard.card.card[ '@type' ].includes('NestedItemCategory')) {
           // Destructuring to make it easy to read.
@@ -45,10 +91,19 @@ const RestaurantMenu = () => {
           const { title, categories } = eachCard.card.card;
           return (
             <div className="item-nested-category" key={index}>
-              <h1 className="p-4 m-4 font-bold">{"**Nested**" + title}</h1>
+              <h1 className="mx-[200px] font-bold">{title + "?"}</h1>
                {categories.map((eachCategory, i) => 
                  <div className="item-nested-category" key={i}>
-                    <h2 className="p-4 m-4 font-semibold">{"**" + eachCategory['title'] + "**"}</h2>
+                   <div className="inline-block mx-[200px]">
+                     <h2 className="font-semibold">
+                       {eachCategory[ 'title' ] + " >"}
+                       <span className="border-b border-solid border-black inline-block w-[10px]"></span>
+                     </h2>
+                   </div>
+                   {/* <h2 className="inline-block mx-[200px] font-semibold border-b border-solid border-black pb-4">{eachCategory[ 'title' ] + " >"}</h2> */}
+                   {/* <div className="inline-block mx-[200px]">
+                     <h2 className="font-semibold">{"hjhjhjhjh"}<span className="border-b border-solid border-black inline-block w-[10px]"></span> &gt;</h2>
+                   </div> */}
                     <ul>
                      {eachCategory[ 'itemCards' ].map((iCards) =>
                        (
